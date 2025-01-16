9c04b685ea0a6cfa8d2cd083b6ea2f7fa9cb8bfe
kp_pfc <krittayos.p@perfectcomsolutions.com>
2025-01-17 01:36:30 +0700

    update: add last-vdo api

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+
+  async getLastVideo() {
+    const playlistId = this.configService.get('LTX_PLAYLIST_ID');
+    const key = this.configService.get('YOUTUBE_API_KEY');
+    try {
+      const playlistItems = await fetch(
+        `https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=${playlistId}&key=${key}`,
+      );
+
+      if (!playlistItems.ok) {
+        throw new Error(`HTTP error! status: ${playlistItems.status}`);
+      }
+
+      const playlistItemsJson = await playlistItems.json(); // Parse the JSON data
+      const vidIds = playlistItemsJson.items.map(
+        (vdo) => vdo?.contentDetails.videoId,
+      );
+      const vidParams = vidIds.join('&id=');
+
+      const videos = await fetch(
+        `https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails,snippet&id=${vidParams}&key=${key}`,
+      );
+      if (!videos.ok) {
+        throw new Error(`HTTP error! status: ${playlistItems.status}`);
+      }
+      const videosJson = await videos.json(); // Parse the JSON data
+      return videosJson;
+    } catch (error) {
+      console.error('Error fetching last seen videos:', error.message);
+      throw new HttpException(
+        'Failed to fetch last seen videos',
+        HttpStatus.BAD_REQUEST,
+        { cause: error },
+      );
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+const allowedOrigins = [
+   'https://missyouluna.vercel.app',
+   'http://localhost:3000',
+]
+
+const corsOptions = {
+   origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+         callback(null, origin) // Allow the origin
+      } else {
+         callback(new Error('Not allowed by CORS')) // Reject the request
+      }
+   },
+   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Allowed methods
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+   const app = await NestFactory.create(AppModule);
+   const configService = app.get<ConfigService>(ConfigService);
+   //NOTE: CORS
+   app.enableCors(corsOptions);
+   //NOTE: Api Version
+   app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: configService.get('API_VERSION') || '1',
+   });
+   //NOTE: Api Prefix
+   app.setGlobalPrefix(configService.get('API_PREFIX') || '/api');
+   app.useGlobalPipes(new ValidationPipe());
+   await app.listen(configService.get('APP_PORT') || 3000);
 }
 bootstrap();
